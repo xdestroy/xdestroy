0ced2b731ae5fbfcb4ad3f24230feccdc9c1577d
Rivvox Kekkies <rorivo@gmail.com>
2012-03-14 22:04:31 +0800

    Signed-off-by: Rivvox Kekkies <rorivo@gmail.com>

--- xdestroy files/Assets/Scripts/Enemy_beh.js
+++ xdestroy files/Assets/Scripts/Enemy_beh.js
@@ -6,33 +6,173 @@ private var movementVector:Vector3;
 
 function ChangeSpeed(inSpeed:float)
 {
-	enemySpeed = inSpeed;
+ enemySpeed = inSpeed;
 }
 
 function Awake()
 {
-	playerReference = GameObject.Find("Player"); // find object 'player'
+ playerReference = GameObject.Find("Player"); // find object 'player'
 
 }
 
 function Update() 
 {
-	if(GameObject.Find("Player") != null) // if player alive follow
-	{
-		UpdateMovement();
-	}
-	else
-	{
-	}
+ if(GameObject.Find("Player") != null) // if player alive follow
+ {
+  UpdateMovement();
+ }
+ else
+ {
+ }
 }
+
+function OnTriggerEnter(collision : Collider)
+{
+ switch(collision.gameObject.name)
+ {
+  case "Wall":
+  print("Enemey Wall Hit");
+  WallCollide();
+  break;
+  default:
+  break;
+ }
+}
+
+ function WallCollide(){
+ var v:Vector3 = this.rigidbody.position;
+
+ if(v.x >= 19)
+ {
+ this.transform.position = Vector3(-19,0,v.z);
+ }
+
+ else if(v.x <= -19)
+ {
+ this.transform.position = Vector3(19,0,v.z); 
+ }
+
+ else if(v.z >= 19)
+ {
+ this.transform.position = Vector3(v.x,0,-19);
+ }
+
+ else if(v.z <= -19)
+ {
+ this.transform.position = Vector3(v.x,0,19);
+ }
+}
+/*
+function UpdateMovement()
+{
+ movementVector = playerReference.transform.position - this.transform.position; // subtract enemy pos from player pos
+ 
+ movementVector.Normalize(); // normalize pos
+ 
+ rigidbody.velocity += 0.03*(movementVector*enemySpeed - rigidbody.velocity); // tells enemy to move to player
+ //current position = itself, plus a portion of the difference between the current position and the target position
+ 
+}
+*/
+
+ /*if (playerX < 0){ // left side
+  pXfromWall =  xylimit + playerX;
+ }else{ //right Side
+   pXfromWall = playerX - xylimit;
+ }
+ if (playerY < 0){ // Top side
+   pYfromWall =  xylimit + playerY;
+ }else{ //Bottom Side
+   pYfromWall = playerY - xylimit;
+ }
+ ///---------------------------------------------------///
+ if (thisX < 0){ // right side
+   tXfromWall = xylimit + thisX;
+ }else{ //left Side
+   tXfromWall = thisX - xylimit;
+ }
+ if (thisY < 0){ // Top side
+   tYfromWall = xylimit + thisY;
+ }else{ //Bottom Side
+   tYfromWall = thisY - xylimit;
+ }
+ 
+ 
+ //distance from enemy to player, via euler   = (playerX+ thisX)
+ //distance via edges         = (pXfromWall + tXfromWall)
+ 
+ 
+ if ( (pXfromWall + tXfromWall) < (playerX + thisX)){
+ tmpVec.x = thisX - playerX;
+ }
+ else{
+ tmpVec.x = thisY + playerY; 
+ }
+ 
+ if ( (pYfromWall + tYfromWall) > (playerY + thisY)){
+ tmpVec.z = thisX - playerX;
+ }
+ else{
+ tmpVec.z = thisY + playerY; 
+ }*/
 
 function UpdateMovement()
 {
-	movementVector = playerReference.transform.position - this.transform.position; // subtract enemy pos from player pos
-	
-	movementVector.Normalize(); // normalize pos
-	
-	rigidbody.velocity += 0.03*(movementVector*enemySpeed - rigidbody.velocity); // tells enemy to move to player
-	//current position = itself, plus a portion of the difference between the current position and the target position
-	
+ var playerX = playerReference.transform.position.x;
+ var playerZ = playerReference.transform.position.z;
+ var thisX = this.transform.position.x;
+ var thisZ = this.transform.position.z;
+ var xylimit = 19.0;
+ var tmpVec = Vector3(0,0,0);
+ var pXfromWall:float;
+ var pYfromWall:float;
+ var tXfromWall:float;
+ var tYfromWall:float;
+ var distanceEPX:float; // distance between en + player for X
+ var distanceEPZ:float; // distance between en + player for Y
+ var MultiX:float; // direction multiplier for X
+ var MultiZ:float; // direction multiplier for Y
+ 
+movementVector = (playerReference.transform.position - this.transform.position);
+ 
+ if(playerX < thisX)
+ {
+	distanceEPX = (thisX - playerX);
+ }
+ else 
+ {
+	distanceEPX = (playerX - thisX);
+}
+if(distanceEPX > xylimit)
+{
+	MultiX = -1;
+}
+else if(distanceEPX < xylimit)
+{
+	MultiX = 1;
+} 
+
+if(playerZ < thisZ)
+ {
+	distanceEPZ = (thisZ - playerZ);
+ }
+ else 
+ {
+	distanceEPZ = (playerZ - thisZ);
+}
+if(distanceEPZ > xylimit)
+{
+	MultiZ = -1;
+}
+else if(distanceEPZ < xylimit)
+{
+	MultiZ = 1;
+}
+ 
+ //movementVector = tmpVec;
+ 
+ movementVector.Normalize(); // normalize pos
+ 
+ rigidbody.velocity += 0.03*(((movementVector*enemySpeed)*MultiX) - rigidbody.velocity); // tells enemy to move to player
+ //current position = itself, plus a portion of the difference between the current position and the target position
 }
