f46297af024435e688ec61c6e15366ea4d4ab3a0
Rivvox Kekkies <rorivo@gmail.com>
2012-03-24 23:12:14 +0800

    Signed-off-by: Rivvox Kekkies <rorivo@gmail.com>

--- xdestroy files - multi spawn enemies/Assets/Scripts/TimeLimitPowerUp_beh.js
+++
@@ -1,9 +0,0 @@
-function Awake()
-{
-	TimeLimit(); // run timelimit script
-}
-function TimeLimit()
-{
-	yield WaitForSeconds(7); // wait for 7 seconds
-	Destroy(gameObject); // destroy powerup if not collected
-}
